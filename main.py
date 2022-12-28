76560b1068ad4f99132032535c05c721a78f41d2
ramdamuser <ramdamuser@gmail.com>
2022-12-28 21:04:25 +1100

    remove blog.db

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ class Comment(db.Model):
     post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
     parent_post = relationship("BlogPost", back_populates="comments")
     text = db.Column(db.Text, nullable=False)
-# db.create_all()
+db.create_all()
 
 
 def admin_only(function):
